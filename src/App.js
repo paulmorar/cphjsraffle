a19df6a700e0584ef35fd0bbd48fbdfc63a53d7e
Paul Morar <paulcornelmorar@gmail.com>
2018-08-15 17:53:05 +0200

    Remove github pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'firebase/database';
-import RaffleForm from './raffle/form.js';
-import RafflePicker from './raffle/picker.js';
+import RaffleForm from './raffle/form';
+import RafflePicker from './raffle/picker';
 import './app.css';
 
 class App extends Component {
