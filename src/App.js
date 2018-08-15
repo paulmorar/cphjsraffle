4fd3e0d6cacd132829b520607657e5d61000b4c8
Paul Morar <paulcornelmorar@gmail.com>
2018-08-15 12:21:05 +0200

    Remove Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'firebase/database';
 import RaffleForm from './raffle/form.js';
 import RafflePicker from './raffle/picker.js';
