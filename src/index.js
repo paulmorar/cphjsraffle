bf1937b375214db4dbe3b89ed0cb398d2aab522d
Paul Morar <paulcornelmorar@gmail.com>
2018-08-15 23:31:43 +0200

    Remove react router and use url params for new entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+import RaffleForm from './raffle/form';
+import RafflePicker from './raffle/picker';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Copied from http:jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+function getUrlVars() {
+  var vars = [],
+    hash;
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf('?') + 1)
+    .split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
+var urlParams = getUrlVars();
+
+switch (urlParams['startPage']) {
+  case 'raffle':
+    ReactDOM.render(<RafflePicker />, document.getElementById('root'));
+    break;
+
+  default:
+    ReactDOM.render(<RaffleForm />, document.getElementById('root'));
+    break;
+}
 
 registerServiceWorker();
